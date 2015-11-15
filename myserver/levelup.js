dcf8f1973b2ada66de1f253d07ff13031dadf3e7
Alejandro Antivero <alejandro.antivero@gmail.com>
2015-11-15 10:33:08 -0300

    LevelDB get, post and delete
    Implement the get, post, delete with LevelDB

--- myserver/levelup.js
+++ myserver/levelup.js
@@ -26,21 +26,7 @@ if ('development' === app.get('env')) {
 }
 
 var db = levelup('./contact', {valueEncoding: 'json'});
-db.put('+359777123456', {
-    "firstname": "Joe",
-    "lastname": "Smith",
-    "title": "Mr.",
-    "company": "Dev Inc.",
-    "jobtitle": "Developer",
-    "primarycontactnumber": "+359777123456",
-    "othercontactnumbers": [
-        "+359777456789",
-        "+359777112233"],
-    "primaryemailaddress": "joe.smith@xyz.com",
-    "emailaddresses": [
-        "j.smith@xyz.com"],
-    "groups": ["Dev","Family"]
-});
+
 app.get('/contacts/:number', function(req, res){
     console.log(req.url + ': querying for ' + req.params.number);
     db.get(req.params.number, function (error, data) {
@@ -52,6 +38,50 @@ app.get('/contacts/:number', function(req, res){
         res.setHeader('content-type', 'application/json');
         res.send(data);
     });
+});
+app.post('/contacts/:number', function (req, res) {
+    console.log('Adding new contact with primary number ' + req.params.number);
+    db.put(req.params.number, req.body, function (error) {
+        if (error) {
+            res.writeHead(500, {'Conten-Type':'text/plain'});
+            res.end('Internal server error');
+            return;
+        }
+        res.send(req.params.number + ' succesfully inserted');
+    });
+});
+app.delete('/contacts/:number', function (req, res) {
+    console.log('Delete contact with primary number ' + req.params.number);
+    db.del(req.params.number, function (error) {
+        if (error) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal server error');
+            return;
+        }
+        res.send(req.params.number + ' successfully deleted');
+    });
+});
+app.get('/contacts', function (req, res) {
+   console.log('Listing all contacts');
+    var is_first = true;
+    res.setHeader('content-type', 'application/json');
+    db.createReadStream().on('data', function (data) {
+        console.log(data.value);
+        if (is_first == true) {
+            res.write('[');
+        } else {
+            res.write(',');
+        }
+        res.write(JSON.stringify(data.value));
+        is_first = false;
+    }).on('error', function (error) {
+        console.log('Error whiling reading. ' + error);
+    }).on('close', function () {
+        console.log('Closing db stream');
+    }).on('end', function () {
+        console.log('Db stream closed');
+        res.end(']');
+    })
 });
 console.log('Running at port ' + app.get('port'));
 http.createServer(app).listen(app.get('port'));
